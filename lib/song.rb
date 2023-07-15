b77daa132c96f46511a1eea24b5458055cdfd5e2
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-07-16 00:28:19 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,25 @@ class Song
 
 
     def initialize(name,artist,genre)
+        @name = name
+        @artist = artist
+        @genre = genre
         @@count += 1
         @@genres << genre
         @@artists << artist
+        
+    end
+
+    def name
+        @name
+    end
+
+    def artist
+        @artist
+    end
+
+    def genre
+        @genre
     end
 
     def self.count
